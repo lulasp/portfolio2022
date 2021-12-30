fc78b5d5005428ff16d167a8557c8b39420d8794
Luis Serpa Pinto <luisserpapinto@deploymentmatters.com>
2021-12-30 03:17:26 +0000

    update gatsby-config & package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Luís Serpa Pinto',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Luís Serpa Pinto is a software developer who specializes in building exceptional digital experiences.',
+    siteUrl: 'https://serpapinto.pt', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'LuisSerpaPinto',
+        short_name: 'LuisSerpaPinto',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
