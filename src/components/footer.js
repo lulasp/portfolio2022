c64fc8d0d8e0022a99ac772e74c6f0a9c981f174
Luis Serpa Pinto <luisserpapinto@deploymentmatters.com>
2022-04-07 17:27:15 +0100

    update footer component. Fixed import warnings.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
